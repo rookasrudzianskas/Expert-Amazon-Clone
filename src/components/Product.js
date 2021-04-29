f594a0ce21a226f9207083dfc314929b7c7e9f75
Rokas <rookas.rudzenskas@gmail.com>
2021-04-29 08:26:56 +0300

    Rokas

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,22 +4,24 @@ import "./styles/Product.css";
 const Product = ({ id, title, image, price, rating }) => {
     return (
         <div className="product">
-            <p>{title}</p>
-            <p className="product__">
-                <small>$</small>
-                <strong>{price}</strong>
-            </p>
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
 
-            <div className="product__rating">
-                {/*stars rating, means ,you create an array, of how many stars are in the product, you fill it with empty values, and map*/}
-                {/* map per each item, it is not important what values, but just run the rating times, and put the start each time*/}
+                <div className="product__rating">
+                    {/*stars rating, means ,you create an array, of how many stars are in the product, you fill it with empty values, and map*/}
+                    {/* map per each item, it is not important what values, but just run the rating times, and put the start each time*/}
 
-                {
-                    Array(rating).fill().map((_) => (
-                        <p>⭐</p>
-                    ))
-                }
-            {/*    end of stars */}
+                    {
+                        Array(rating).fill().map((_) => (
+                            <p>⭐</p>
+                        ))
+                    }
+                {/*    end of stars */}
+                </div>
             </div>
             {/* the description image */}
             <img src={image} alt=""/>
