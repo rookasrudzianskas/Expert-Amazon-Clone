949e16e65cbf3dac1c5e9393b8618103a1f885ea
Rokas <rookas.rudzenskas@gmail.com>
2021-04-29 06:07:26 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from "react";
+
 
 function App() {
   return (
     <div className="app">
         {/* start */}
         <h1>Hello world 🚀</h1>
+
+    {/*    local amazon.com     */}
+    {/*    localhost.com/checkout   */}
+    {/*    localhost.com/login   */}
+
     </div>
   );
 }
