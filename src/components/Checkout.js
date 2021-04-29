834ea47cd29f2f59695f9af7a0d2e22a366f0c6f
Rokas <rookas.rudzenskas@gmail.com>
2021-04-29 17:20:22 +0300

    rokas

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -13,6 +13,9 @@ const Checkout = () => {
             {basket?.length === 0 ? (
                 <div>
                     <h2>Your shopping basket is empty</h2>
+                    <p>
+                        You have no items in your basket. To buy one or more items, just click "Add to basket" next to the item
+                    </p>
                 </div>
             ) : (
                 <div>
