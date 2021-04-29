2af89c97e2d6db05667802ed76f7666920b57902
Rokas <rookas.rudzenskas@gmail.com>
2021-04-29 07:17:03 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,53 @@ const Header = () => {
                 <input type="text" className="header__searchInput" />
                 <SearchIcon className="header__searchIcon" />
             </div>
+
+        {/*    end of search box    */}
         {/*    3 links  */}
+
+            <div className="header__nav">
+            {/*    1 link */}
+
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span>Hello, Rokas</span>
+                        <span>Sing In</span>
+                    </div>
+                </Link>
+
+            {/*    end of 1 link */}
+
+            {/*     2 link */}
+
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span>Returns</span>
+                        <span>& Orders</span>
+                    </div>
+                </Link>
+
+            {/*    end of 2 link */}
+
+            {/*     3 link */}
+
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span>Your</span>
+                        <span>Prime</span>
+                    </div>
+                </Link>
+
+            {/*    end of 3 link */}
+
+            {/*     4 link */}
+
+            {/*     end of 4 link */}
+
+
+            </div>
+
+        {/*    end of 3 links   */}
+
         {/*    Basket icon with the number   */}
 
         </nav>
