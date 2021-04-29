3bb5daf0b913021fe618c26f0abf1ec68af6ac45
Rokas <rookas.rudzenskas@gmail.com>
2021-04-29 08:34:53 +0300

    Rokas

--- src/components/Home.js
+++ src/components/Home.js
@@ -29,13 +29,53 @@ const Home = () => {
                         image="https://images-na.ssl-images-amazon.com/images/I/81O%2BGNdkzKL._AC_SX450_.jpg"
                 />
 
+            </div>
 
+            {/* Product  row 2 */}
+
+            <div className="home__row">
+
+                <Product
+                    id="4903850"
+                    title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor"
+                    price={199.99}
+                    rating={3}
+                    image="https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg"
+                />
+                <Product
+                    id="23445930"
+                    title="Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric"
+                    price={98.99}
+                    rating={5}
+                    image="https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$"
+                />
+                <Product
+                    id="3254354345"
+                    title="New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)"
+                    price={598.99}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/816ctt5WV5L._AC_SX385_.jpg"
+                />
 
             </div>
 
-            {/* Product */}
+            {/* end of Product  row 2 */}
 
+            {/* Product  row 3 */}
 
+            <div className="home__row">
+
+                <Product
+                    id="90829332"
+                    title="Samsung LC49RG90SSUXEN 49' Curved LED Gaming Monitor - Super Ultra Wide Dual WQHD 5120 x 1440"
+                    price={1094.98}
+                    rating={4}
+                    image="https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg"
+                />
+
+            </div>
+
+            {/* end of Product  row 3 */}
         {/*    end of products  */}
         </div>
     );
